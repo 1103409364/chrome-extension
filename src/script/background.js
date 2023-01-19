628603c8e30df99cb77ce825b5bfce0f4e017c99
1103409364 <1103409364@qq.com>
2023-01-19 14:47:26 +0800

    doc: 添加注释
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/script/background.js
+++ src/script/background.js
@@ -1,5 +1,9 @@
 chrome.action.onClicked.addListener((tab) => {
+  // 点击扩展按钮图标。配置了manifest.json "default_popup": "src/views/popup/main.html", 将无法触发 click
+  console.log("action click", tab);
   if (tab.id) {
-    chrome.tabs.sendMessage(tab.id, { toggleVisible: true });
+    // chrome.tabs.sendMessage(tab.id, { toggleVisible: true });
   }
 });
+
+console.log("background.js");
